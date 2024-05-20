150f94406a192af5ecd1e548f1dc1d7979a11262
xuand <xuandangvinh@gmail.com>
2024-05-20 18:31:43 +0700

    first commit

---
+++ Test_How to using git in Jetbrain/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Test_How_to_using_git_in_Jetbrain;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
